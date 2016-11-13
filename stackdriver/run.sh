0b0480af9020f37a404f28a9d5c752ee93eb19b9
Mikael Keto <mikael@fameads.com>
2016-11-13 16:35:17 +0100

    Removed API key for stackdriver.

--- stackdriver/run.sh
+++ stackdriver/run.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
-if [ -z "$api_key" ]; then
-    echo "Enviroment variable api_key not found!"
-    exit 1
-fi
-
-/opt/stackdriver/stack-config --api-key=$api_key --write-gcm --no-start
+/opt/stackdriver/stack-config --write-gcm --no-start
 /etc/init.d/stackdriver-agent start
 
 while true; do
 	sleep 60
 	agent_pid=$(cat /var/run/stackdriver-agent.pid 2>/dev/null)
-	extractor_pid=$(cat /var/run/stackdriver-extractor.pid 2>/dev/null)
 
 	ps -p $agent_pid > /dev/null 2>&1
 	if [ $? != 0 ]; then
 		echo "Stackdriver agent pid not found!"
-		break;
-	fi
-
-	ps -p $extractor_pid > /dev/null 2>&1
-	if [ $? != 0 ]; then
-		echo "Stackdriver extractor pid not found!"
 		break;
 	fi
 done
